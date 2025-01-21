12b2bc0d79d1e6155e5fdd16f99ef72b321895ce
Johnsonlind <152807479+Johnsonlind@users.noreply.github.com>
2025-01-21 22:43:22 +0800

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,6 @@
 export const TMDB_API_KEY = '4f681fa7b5ab7346a4e184bbf2d41715';
 export const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
-export const TMDB_IMAGE_BASE_URL = 'https://image.tmdb.org/t/p';
+export const TMDB_IMAGE_BASE_URL = "https://image.tmdb.org";
 
 export const POSTER_SIZES = {
   small: 'w185',
