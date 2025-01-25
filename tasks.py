6f7d7085496c5aa5ac36668ad8af78d52a6e9de0
Johnsonlind <152807479+Johnsonlind@users.noreply.github.com>
2025-01-26 04:00:01 +0800

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -3,6 +3,7 @@ from dramatiq.brokers.redis import RedisBroker
 from ratings import extract_rating_info
 from dramatiq.results import Results
 from dramatiq.results.backends import RedisBackend
+from dramatiq.middleware import AsyncIO
 
 # 配置 Redis results backend
 result_backend = RedisBackend(url="redis://:l1994z0912x@localhost:6379/0")
@@ -10,6 +11,7 @@ result_backend = RedisBackend(url="redis://:l1994z0912x@localhost:6379/0")
 # Redis broker 配置
 redis_broker = RedisBroker(url="redis://:l1994z0912x@localhost:6379/0")
 redis_broker.add_middleware(Results(backend=result_backend))
+redis_broker.add_middleware(AsyncIO())
 
 dramatiq.set_broker(redis_broker)
 
