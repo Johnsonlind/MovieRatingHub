d67dc23dfd45662a967a134d004c2138680e43a6
Johnsonlind <152807479+Johnsonlind@users.noreply.github.com>
2025-02-27 07:37:39 +0800

    Delete src directory

--- src/utils/typeGuards.ts
+++
@@ -1,5 +0,0 @@
-import type { MovieRatingData, TVShowRatingData } from '../types/ratings';
-
-export function isTVShowRatingData(data: MovieRatingData | TVShowRatingData): data is TVShowRatingData {
-  return data.type === 'tv';
-} 
