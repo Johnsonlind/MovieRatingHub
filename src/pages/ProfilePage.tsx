bbbcab6319bcdcfe53f0a79055c81318d5243053
Johnsonlind <152807479+Johnsonlind@users.noreply.github.com>
2025-03-07 16:03:08 +0800

    Update ProfilePage.tsx

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -816,9 +816,15 @@ export default function ProfilePage() {
               </button>
             </div>
             {activeTab === 'collections' && (
-              <Button onClick={() => setShowCreateDialog(true)}>
-                创建收藏列表
-              </Button>
+              <button
+                onClick={() => setShowCreateDialog(true)}
+                className="p-2 text-gray-500 hover:text-blue-600 rounded-full hover:bg-gray-100 dark:hover:bg-gray-700 transition-colors"
+                title="创建收藏列表"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clipRule="evenodd" />
+                </svg>
+              </button>
             )}
           </div>
           
